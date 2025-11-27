c3392db755214fdf6e21cf274374f6b7618267cd
Raguraman Asokan <raguraman.asokan@gmail.com>
2025-11-27 09:24:39 +0300

    Initial Commit

---
+++ BottleDetector/BottleDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottleDetectorApp.swift
+//  BottleDetector
+//
+//  Created by Raguraman on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BottleDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
